5185dd1f4d49c75bd9265e872be8d06e895f54a2
Suyog28 <suyogvmuley@gmail.com>
2023-02-12 01:29:14 +0530

    Menu Item functionality added

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { restaurantList } from "../config";
 import RestaurantCard from "./RestaurantCard"
 import Shimmer from "./Shimmer";
+import { Link } from "react-router-dom";
 
 function filterData(searchText, restaurants) {
     const filterData = restaurants.filter((restaurant) =>
@@ -55,7 +56,9 @@ const Body = () => {
                         <h1>No Restaurant Found..! Type Something Else..</h1>
                     ) : (
                         filteredRestaurants.map((restaurant) => {
-                            return < RestaurantCard {...restaurant.data} key={restaurant.data.id} />
+                            return <Link to={"/restaurant/" + restaurant.data.id} key={restaurant.data.id}>
+                                < RestaurantCard {...restaurant.data} />
+                            </Link>
 
                         })
                     )}
--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -1,15 +1,48 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { IMG_CDN_URL } from "../config";
+import Shimmer from "./Shimmer";
 
 const RestaurantMenu = () => {
     const { id } = useParams();//Smart/Cool developer use this method
+    // const { id } = param; distrucring
+    // console.log(id);
+    const [restaurant, setRestaurant] = useState(null);
 
-    // const { id } = param; distrucring 
+    useEffect(() => {
+        getRestaurantInfo();
+    }, []);
 
-    console.log(id);
+    async function getRestaurantInfo() {
+        const data = await fetch("https://www.swiggy.com/dapi/menu/v4/full?lat=12.9351929&lng=77.62448069999999&menuId=" + id);
+        const json = await data.json();
+        console.log(json.data);
+        setRestaurant(json.data)
+    }
 
-    return (<div>
-        <h1>restaurant:{id}</h1>
-    </div>)
+    return (!restaurant) ? <Shimmer /> : (
+        <>
+            <div className="menu">
+                <div>
+                    <h1>Restraunt id: {id}</h1>
+                    <h2>{restaurant.name}</h2>
+                    <img src={IMG_CDN_URL + restaurant.cloudinaryImageId} />
+                    <h3>{restaurant?.area}</h3>
+                    <h3>{restaurant?.city}</h3>
+                    <h3>{restaurant?.avgRating} stars</h3>
+                    <h3>{restaurant?.costForTwoMsg}</h3>
+                </div>
+                <div>
+                    <h1>Menu</h1>
+                    <ul>
+                        {Object.values(restaurant.menu.items).map((item) => <li key={item.id}>{item.name}</li>)}
+                    </ul>
+
+                </div>
+            </div>
+        </>
+
+    )
 
 }
 
